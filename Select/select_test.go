e7c0973aac1ea097248256a6289c297f82cf2aa2
Renu <admin@Admins-MacBook-Pro.local>
2024-12-08 16:03:11 +0530

    Practiced Reflection : Knowing reflection is knowledge, not using it is a wisdom

--- Select/select_test.go
+++ Select/select_test.go
@@ -11,6 +11,7 @@ func makeDelayedServer(delay time.Duration) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(delay)
 		w.WriteHeader(http.StatusOK)
+
 	}))
 }
 
